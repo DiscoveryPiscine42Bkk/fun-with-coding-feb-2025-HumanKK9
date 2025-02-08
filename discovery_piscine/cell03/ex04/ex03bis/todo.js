6820ee26fe387fc39ce9d7695ec9d4a825c3b8ef
HumanKK9 <154393014+HumanKK9@users.noreply.github.com>
2025-02-08 23:07:32 +0700

    Add files via upload

--- discovery_piscine/cell03/ex04/ex03bis/todo.js
+++ discovery_piscine/cell03/ex04/ex03bis/todo.js
@@ -6,7 +6,7 @@ window.onload = function() {
     let decodedCookie = decodeURIComponent(document.cookie);;
     let ca = decodedCookie.split(';');;
     m=0;
-    for(i=0; i <ca.length; i++) {
+    for(i=1; i <ca.length; i++) {
         let c = ca[i];;
         while (c.charAt(0) == ' ') {
         c = c.substring(1);;
@@ -73,54 +73,3 @@ function DES_TROY(justalilguy) {
     }     
 }
 
-
-//-------------------------
-
-
-function actofcreation() {
-    stuffs=prompt("Engage in the act of creation, fool.", "make a little guy!")
-    makealittleguy(stuffs,i);
-    i=i+1;
-}
-function makealittleguy(stuffs,theirnumber) {
-    const thing = document.createElement("div");
-    thing.innerHTML = stuffs;
-    thing.id=theirnumber;
-    thing.onclick=function () {DES_TROY(this.id);};
-    thing.style.border="1px black";
-    thing.style.margin="2px";
-    thing.style.padding="5px";
-    thing.style.position="absolute";
-    document.getElementById("ft_list").appendChild(thing);
-    h=i;
-    for(h=i; h!=-1; h=h-1) {
-        fall=((i-h)*50)+50+"px";
-        if(document.getElementById(h)){
-            document.getElementById(h).style.top=fall;
-        }
-        
-    }
-    document.cookie = theirnumber.toString() + "=" + stuffs + ";path=/";
-}
-function DES_TROY(justalilguy) {
-    if (i!=justalilguy){
-        confirmation=confirm("Destroy this creature?");
-        if (confirmation==true ){
-            victim = document.getElementById(justalilguy);
-            victim.remove();
-            alert("Your EXP increased.")
-            h=i;
-            for(h=i; h!=-1; h=h-1) {
-                fall=((i-h-1)*50)+50+"px";
-                if(document.getElementById(h)){
-                    document.getElementById(h).style.top=fall;
-                }
-                
-            }
-            document.cookie = justalilguy+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";//EXPIRE, CREATURE
-        }
-        else {
-            alert("The creature is relieved for being able to live for just one more second.")
-        }
-    }     
-}
