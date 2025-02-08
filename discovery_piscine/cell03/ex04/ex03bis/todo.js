acb9058766e0b75da5047883589c55a57de7afca
HumanKK9 <154393014+HumanKK9@users.noreply.github.com>
2025-02-08 23:06:10 +0700

    Add files via upload

--- discovery_piscine/cell03/ex04/ex03bis/todo.js
+++ discovery_piscine/cell03/ex04/ex03bis/todo.js
@@ -1,6 +1,5 @@
 i=0
 window.onload = function() {
-    alert(document.cookie)
     i=0;
     if(document.cookie!="undefined")
     {
@@ -16,7 +15,6 @@ window.onload = function() {
         idname=c.split('=')[0];
         numbe=Number(idname);     
         word=c.split('=')[1]
-        alert(c.split('=')[0])
         makealittleguy(c.split('=')[1],idname);;//need to find a way to get number of characters of the id inside the string in cookie
         if(numbe>m){
             m=numbe;
@@ -36,6 +34,7 @@ function makealittleguy(stuffs,theirnumber) {
     thing.innerHTML = stuffs;
     thing.id=theirnumber;
     thing.style.border="1px black";
+    thing.onclick=function () {DES_TROY(this.id);};
     thing.style.margin="2px";
     thing.style.padding="5px";
     thing.style.position="absolute";
@@ -51,8 +50,7 @@ function makealittleguy(stuffs,theirnumber) {
     document.cookie = theirnumber.toString() + "=" + stuffs + ";path=/";
 }
 
-$("div").click(function(){
-    justalilguy=$(this).attr('id');
+function DES_TROY(justalilguy) {
     if (i!=justalilguy){
         confirmation=confirm("Destroy this creature?");
         if (confirmation==true ){
@@ -73,4 +71,56 @@ $("div").click(function(){
             alert("The creature is relieved for being able to live for just one more second.")
         }
     }     
-})
+}
+
+
+//-------------------------
+
+
+function actofcreation() {
+    stuffs=prompt("Engage in the act of creation, fool.", "make a little guy!")
+    makealittleguy(stuffs,i);
+    i=i+1;
+}
+function makealittleguy(stuffs,theirnumber) {
+    const thing = document.createElement("div");
+    thing.innerHTML = stuffs;
+    thing.id=theirnumber;
+    thing.onclick=function () {DES_TROY(this.id);};
+    thing.style.border="1px black";
+    thing.style.margin="2px";
+    thing.style.padding="5px";
+    thing.style.position="absolute";
+    document.getElementById("ft_list").appendChild(thing);
+    h=i;
+    for(h=i; h!=-1; h=h-1) {
+        fall=((i-h)*50)+50+"px";
+        if(document.getElementById(h)){
+            document.getElementById(h).style.top=fall;
+        }
+        
+    }
+    document.cookie = theirnumber.toString() + "=" + stuffs + ";path=/";
+}
+function DES_TROY(justalilguy) {
+    if (i!=justalilguy){
+        confirmation=confirm("Destroy this creature?");
+        if (confirmation==true ){
+            victim = document.getElementById(justalilguy);
+            victim.remove();
+            alert("Your EXP increased.")
+            h=i;
+            for(h=i; h!=-1; h=h-1) {
+                fall=((i-h-1)*50)+50+"px";
+                if(document.getElementById(h)){
+                    document.getElementById(h).style.top=fall;
+                }
+                
+            }
+            document.cookie = justalilguy+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";//EXPIRE, CREATURE
+        }
+        else {
+            alert("The creature is relieved for being able to live for just one more second.")
+        }
+    }     
+}
